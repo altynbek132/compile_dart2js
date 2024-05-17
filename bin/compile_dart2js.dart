88ddbdda6b57c26e1c01ac44d61b0708dc33b657
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-05-17 17:32:27 +0500

    refactor

--- bin/compile_dart2js.dart
+++ bin/compile_dart2js.dart
@@ -2,25 +2,33 @@ import 'package:args/args.dart';
 
 const String version = '0.0.1';
 
+abstract class Args {
+  static const String help = 'help';
+  static const String verbose = 'verbose';
+  static const String version = 'version';
+}
+
 ArgParser buildParser() {
   return ArgParser()
-    ..addFlag(
-      'help',
-      abbr: 'h',
-      negatable: false,
-      help: 'Print this usage information.',
-    )
-    ..addFlag(
-      'verbose',
-      abbr: 'v',
-      negatable: false,
-      help: 'Show additional command output.',
-    )
-    ..addFlag(
-      'version',
-      negatable: false,
-      help: 'Print the tool version.',
-    );
+        ..addFlag(
+          Args.help,
+          abbr: 'h',
+          negatable: false,
+          help: 'Print this usage information.',
+        )
+        ..addFlag(
+          Args.verbose,
+          abbr: 'v',
+          negatable: false,
+          help: 'Show additional command output.',
+        )
+        ..addFlag(
+          Args.version,
+          negatable: false,
+          help: 'Print the tool version.',
+        )
+      //
+      ;
 }
 
 void printUsage(ArgParser argParser) {
@@ -35,15 +43,15 @@ void main(List<String> arguments) {
     bool verbose = false;
 
     // Process the parsed arguments.
-    if (results.wasParsed('help')) {
+    if (results.wasParsed(Args.help)) {
       printUsage(argParser);
       return;
     }
-    if (results.wasParsed('version')) {
+    if (results.wasParsed(Args.version)) {
       print('compile_dart2js version: $version');
       return;
     }
-    if (results.wasParsed('verbose')) {
+    if (results.wasParsed(Args.verbose)) {
       verbose = true;
     }
 
