36bb98326fa61d5b229b75849de27f5204fc1cc1
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-05-18 00:13:06 +0500

    test: Add tests for compileDart2Js function

---
+++ test/compile_dart2_js_test.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import 'package:compile_dart2js/compile_dart2_js.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('compileDart2Js', () {
+    const assetsDir = 'test/assets';
+
+    setUp(() {
+      cleanNonDart(assetsDir);
+    });
+
+    tearDownAll(() {
+      cleanNonDart(assetsDir);
+    });
+
+    test('should compile a single file', () async {
+      await compileDart2Js(assetsDir, 'main.dart', fvm: true);
+      expect(await File('$assetsDir/main.dart.js').exists(), true);
+      expect(await File('$assetsDir/main2.dart.js').exists(), false);
+    });
+    test('should handle non-existent directory', () async {
+      const dir = '/path/to/nonexistent/directory';
+      const globPattern = '*.dart';
+
+      await expectLater(
+        () => compileDart2Js(dir, globPattern, fvm: true),
+        throwsA(isA<FileSystemException>()),
+      );
+    });
+
+    test('should compile multiple files', () async {
+      const globPattern = '*.dart';
+
+      await compileDart2Js(assetsDir, globPattern, fvm: true);
+
+      expect(await File('$assetsDir/main.dart.js').exists(), true);
+      expect(await File('$assetsDir/main2.dart.js').exists(), true);
+    });
+    test('should obey glob pattern', () async {
+      const globPattern = '*_compile_js.dart';
+
+      await compileDart2Js(assetsDir, globPattern, fvm: true);
+
+      expect(await File('$assetsDir/foo_compile_js.dart.js').exists(), true);
+      expect(await File('$assetsDir/bar_compile_js.dart.js').exists(), true);
+
+      expect(await File('$assetsDir/main.dart.js').exists(), false);
+      expect(await File('$assetsDir/main2.dart.js').exists(), false);
+    });
+  });
+}
+
+void cleanNonDart(String assetsDir) {
+  final files = Directory(assetsDir).listSync();
+  for (final file in files) {
+    if (!file.path.endsWith('.dart')) {
+      file.deleteSync();
+    }
+  }
+}
